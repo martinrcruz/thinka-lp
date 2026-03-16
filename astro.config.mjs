3514a1c38ad9e24027cd045abff3a84b26ac3835
martinrcruz <martinignacioriosc@gmail.com>
2026-03-16 19:36:39 -0300

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://thinka.cl', // Base URL required for sitemap
+  integrations: [tailwind(), sitemap()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
+});
